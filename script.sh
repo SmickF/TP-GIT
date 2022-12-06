934d9ef68f5ec7d5e25cfed59345078afd5ff354
Miguel <fernandesmiguel@outlook.com>
2022-12-06 21:37:22 +0100

    V1 Add user and key

--- script.sh
+++ script.sh
@@ -7,8 +7,24 @@ cp ./config_files/ssh/sshd_config /etc/ssh/sshd_config
 
 cp ./config_files/ssh/Banner /etc/Banner
 
+# Création utilisateur
+
+echo "Nom d'utilisateur :"
+read username
+
+useradd -m -d/home/$username -s /bin/bash -c "$username" $username
+passwd $username
+usermod -aG sudo $username
+
+
+# Insertion clé public
+
+echo "Clé SSH :"
+read key_ssh
+
+echo $key_ssh >> ~/.ssh/authorized_keys
+
 
 # Redemarrer le service sshd 
 
 systemctl restart sshd 
-
